01a640f63512fb12927fc2021dbc6688ac97aa6c
nakayamahiroshi164 <box.xx164@gmail.com>
2023-11-21 15:49:49 +0900

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
+  before do
+    @user = FactoryBot.build(:user)
+  end
   
   describe 'ユーザー新規登録' do
     it 'nameが空では登録できない' do
-      user = FactoryBot.build(:user)  # Userのインスタンス生成
-      user.name = ''  # nicknameの値を空にする
-      user.valid?
-      expect(user.errors.full_messages).to include("Nameを入力してください")
+      @user.name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nameを入力してください")
     end
     it 'emailが空では登録できない' do
-      user = FactoryBot.build(:user)  # Userのインスタンス生成
-      user.email = ''  # emailの値を空にする
-      user.valid?
-      expect(user.errors.full_messages).to include("Eメールを入力してください")
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Eメールを入力してください")
     end
   end
 end
