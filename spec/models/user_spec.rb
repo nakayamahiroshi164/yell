b2ae08bd3e6cbcd972f4e216cb3a5526f1ba2f0b
nakayamahiroshi164 <box.xx164@gmail.com>
2023-11-29 22:09:33 +0900

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,22 +9,59 @@ RSpec.describe User, type: :model do
     it 'nameとemail、passwordとpassword_confirmationが存在すれば登録できる' do
     end
     it 'nameが空では登録できない' do
+      @user.name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nameを入力してください")
+
     end
     it 'emailが空では登録できない' do
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Eメールを入力してください")
+
     end
     it 'passwordが空では登録できない' do
+      @user.password = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("パスワードを入力してください", "パスワード（確認用）とパスワードの入力が一致しません", "パスワードは6文字以上で入力してください")
+
     end
     it 'passwordとpassword_confirmationが不一致では登録できない' do
+      @user.password = '123456'
+      @user.password_confirmation = '1234567'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("パスワード（確認用）とパスワードの入力が一致しません")
+
     end
     it 'nameが7文字以上では登録できない' do
+      @user.name = 'aaaaaaa'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nameは6文字以内で入力してください")
+
     end
     it '重複したemailが存在する場合は登録できない' do
+      @user.save
+      another_user = FactoryBot.build(:user)
+      another_user.email = @user.email
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include("Eメールはすでに存在します")
+
     end
     it 'emailは@を含まないと登録できない' do
+      @user.email = 'testmail'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Eメールは不正な値です")
+
     end
     it 'passwordが5文字以下では登録できない' do
+      @user.password = '00000'
+      @user.password_confirmation = '00000'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("パスワードは6文字以上で入力してください", "パスワード（確認用）は6文字以上で入力してください")
+  
     end
     it 'passwordが30文字以上では登録できない' do
     end
-  end
+    
+    end
 end
